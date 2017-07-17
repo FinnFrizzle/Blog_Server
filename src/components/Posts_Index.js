58432a8e08a4d89dd2dd7181227f599d1061fbb8
Eugene Sokol <Eugenesokol97@gmail.com>
2017-07-17 16:25:09 +0200

    use of fetched data - error

--- src/components/Posts_Index.js
+++ src/components/Posts_Index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
-export default class PostsIndex extends React.Component {
+class PostsIndex extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+
   render() {
     return (
       <div> PostIndex </div>
     );
   }
 }
+
+export default connect(null, {fetchPosts}) (PostsIndex);
