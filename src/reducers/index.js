dacbb7af4a24d5886e5510fcde05953ef68d717c
Eugene Sokol <Eugenesokol97@gmail.com>
2017-07-17 15:23:57 +0200

    Minor change

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import PostsReducer from './reducers/Reducer_Posts.js';
+import PostsReducer from './Reducer_Posts.js';
 
 const rootReducer = combineReducers({
-  posts: PostReducer
+  posts: PostsReducer
 });
 
 export default rootReducer;
