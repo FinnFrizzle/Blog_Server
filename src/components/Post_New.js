0f6eecee70d7abfd24e15531400fd22ecab76d82
Eugene Sokol <Eugenesokol97@gmail.com>
2017-07-18 19:09:08 +0200

    Added form validation (details)
    form-validation
    +
    Error from last commit not fixed,
    but there came a new error to overide it lol ('TypeError: Cannot read property 'handleSubmit' of undefined') - it should be defined at line 28.

--- src/components/Post_New.js
+++ src/components/Post_New.js
@@ -1,26 +1,71 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
-function renderTitleField(field) {
+function renderField(field) {
   return (
-    <div>
-      <input type="type"
+    <div className="form-group">
+      <label> {field.label} </label>
+      <input
+        className="form-control"
+        type="type"
         { ...field.input }
       />
+      <div className="text-help">
+        {field.meta.touched ? field.meta.error : ''}
+      </div>
     </div>
   );
 }
 
 class PostNew extends React.Component {
+  // our own side of the submit, to save the data etc.
+  onSubmit(values) {
+    console.log(values)
+  }
+
   render() {
+    // let redux-form handle validation etc (this.probs is comming form redux-form)
+    const { handleSubmit } = this.probs;
+
     return (
-      <form>
-        <Field name="title" component={this.renderTitleField} />
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label='Title'
+          name="title"
+          component={this.renderField} />
+        <Field
+          label='Categories'
+          name="categories"
+          component={this.renderField} />
+        <Field
+          label='Contents'
+          name="contents"
+          component={this.renderField} />
+        <button type="submit" className="btn-primary"> Submit </button>
       </form>
     );
   }
 }
 
+function validate(value) {
+  const error = {};
+  // validate
+  if (!value.title) {
+    error.title = "Enter a title-name"
+  }
+  if (!value.content) {
+    error.categories = "Enter some categories"
+  }
+  if (!value.contents) {
+    error.title = "Enter some contents"
+  }
+
+
+  // if error = {}, form is valid! If any other stuff, not vaild!
+  return error;
+}
+
 export default reduxForm({
+  validate,
   form: "postNewForm"
 })(PostNew);
